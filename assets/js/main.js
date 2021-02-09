f851ebda61f8765de15065c4a900a90f4a53e815
Jose Alamos <jose@alamos.cc>
2021-02-09 17:55:27 +0100

    animate counters

--- assets/js/main.js
+++ assets/js/main.js
@@ -247,6 +247,21 @@
       once: true,
       mirror: false
     })
-  });
+});
 
+  /**
+   * Extracted from
+   * https://www.i-visionblog.com/2014/11/jquery-animated-number-counter-from-zero-to-value-jquery-animation.html
+   */
+     $('.count').each(function () {
+        $(this).prop('Counter',0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 1500,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    }); 
 })()
