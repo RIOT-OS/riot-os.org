470ee8b262e7ff233d4f3909230461753d22e11e
Leandro Lanzieri <leandro.lanzieri@haw-hamburg.de>
2021-02-09 19:16:26 +0100

    index: trigger counters on scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -249,19 +249,22 @@
     })
 });
 
-  /**
-   * Extracted from
-   * https://www.i-visionblog.com/2014/11/jquery-animated-number-counter-from-zero-to-value-jquery-animation.html
-   */
-     $('.count').each(function () {
-        $(this).prop('Counter',0).animate({
-            Counter: $(this).text()
-        }, {
-            duration: 1500,
-            easing: 'swing',
-            step: function (now) {
-                $(this).text(Math.ceil(now));
-            }
-        });
-    }); 
+
+  document.addEventListener('aos:in:countUp', ({ detail }) => {
+    /**
+      * Extracted from
+      * https://www.i-visionblog.com/2014/11/jquery-animated-number-counter-from-zero-to-value-jquery-animation.html
+    */
+    $('.count').each(function () {
+      $(this).prop('Counter',0).animate({
+          Counter: $(this).text()
+      }, {
+          duration: 1500,
+          easing: 'swing',
+          step: function (now) {
+              $(this).text(Math.ceil(now));
+          }
+      });
+    });
+  });
 })()
