e49284ce2b13071c368d1affcc7002e74fe8a992
Jose Alamos <jose@alamos.cc>
2021-02-26 16:07:56 +0100

    fixup! tools: add drivers parser

--- _tools/riot-drivers-cats.sh
+++ _tools/riot-drivers-cats.sh
@@ -9,9 +9,19 @@
 RIOTBASE=$1
 
 parsedriverscats () {
-    cd $RIOTBASE
-    echo "group,name"; git grep -h -A1 -B1 defgroup drivers/doc.txt | awk -F " " '/defgroup\s+drivers_/ {match($0, "defgroup\\s+(\\w+)\\s*(.*$)", a); print a[1] "," a[2]}'
-    cd - > /dev/null
+    git -C $RIOTBASE grep -h -A1 -B1 defgroup drivers/doc.txt | \
+        awk -F " " '
+            # Add CSV header
+            BEGIN {
+                print "group,name"
+            } 
+
+            # Extract group and name from the defgroup tag
+            /defgroup\s+drivers_/ {
+                match($0, "defgroup\\s+(\\w+)\\s*(.*$)", a);
+                print a[1] "," a[2]
+            }
+        '
 }
 
 parsedriverscats
