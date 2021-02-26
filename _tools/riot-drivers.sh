9bf19eec5bc86b58839dd1d4860d41bc988e2b40
Jose Alamos <jose@alamos.cc>
2021-02-26 16:08:01 +0100

    fixup! tools: add drivers parser

--- _tools/riot-drivers.sh
+++ _tools/riot-drivers.sh
@@ -8,10 +8,32 @@
 
 RIOTBASE=$1
 
+# Get the driver groups from Doxygen's defgroup tag
+listdrivergroups () {
+    git -C $RIOTBASE grep defgroup drivers/doc.txt | \
+        awk -F " " '/defgroup\s+drivers_/ {print $4}' 
+}
+
+listbytype () {
+    git -C $RIOTBASE grep -h -B 5 -A 5 "ingroup\s*$1\$" | grep defgroup
+}
+
 parsedrivers () {
-    cd $RIOTBASE
-    echo "parent,group,name";git grep defgroup drivers/doc.txt | awk -F " " '/defgroup\s+drivers_/ {print $4}' | xargs -I % sh -c 'git grep -h -B 5 -A 5 "ingroup\s*%$" | awk "{print \"%:\" \$0}"' | grep defgroup | awk '{match($0, "(^.*):.*defgroup\\s+(\\w*)\\s*(.*$)",a); print a[1] "," a[2] "," a[3]}'
-    cd - > /dev/null
+    echo "parent,group,name"
+
+    for group in $(listdrivergroups)
+    do
+        listbytype "$group" | \
+            awk '
+                    {
+                        # Extract driver group and name
+                        match($0, "defgroup\\s+(\\w*)\\s*(.*$)",a);
+                        
+                        # Create CSV (parent, group, name)
+                        print parent "," a[1] "," a[2]
+                    }
+                ' parent=$group
+    done
 }
 
 parsedrivers
